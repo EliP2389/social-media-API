5e702d99413923e727737decafa342fd159c56f8
Eli Pagan <mbk8689@gmail.com>
2022-02-08 10:24:56 -0500

    created user schema

--- models/User.js
+++ models/User.js
@@ -1,2 +1,45 @@
 const { Schema, model, Types } = require('mongoose');
-const dateFormat = require()
+const dateFormat = require('../utils/dateFormat')
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+       thoughts: [
+           {
+               type: Schema.Types.ObjectId,
+               ref: 'Thoughts'
+           }
+       ],
+       friends: [
+           {
+               type: Schema.Types.ObjectId,
+               ref: 'User'
+           }
+       ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.reduce((total, friend) => total + friend.user.length + 1, 0);
+});
+
+const User = model('User', UserSchema);
+
+model.exports = Pizza;
--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,7 +1,7 @@
 const addDateSuffix = date => {
     let dateStr = date.toString();
 
-    
+
     const lastChar = dateStr.charAt(dateStr.length - 1);
 
     if (lastChar === '1' && dateStr !== '11') {
@@ -25,32 +25,32 @@ module.exports = (
     if (monthLength === 'short') {
         months = {
             0: 'Jan',
-      1: 'Feb',
-      2: 'Mar',
-      3: 'Apr',
-      4: 'May',
-      5: 'Jun',
-      6: 'Jul',
-      7: 'Aug',
-      8: 'Sep',
-      9: 'Oct',
-      10: 'Nov',
-      11: 'Dec'
+            1: 'Feb',
+            2: 'Mar',
+            3: 'Apr',
+            4: 'May',
+            5: 'Jun',
+            6: 'Jul',
+            7: 'Aug',
+            8: 'Sep',
+            9: 'Oct',
+            10: 'Nov',
+            11: 'Dec'
         };
     } else {
         months = {
             0: 'January',
-      1: 'February',
-      2: 'March',
-      3: 'April',
-      4: 'May',
-      5: 'June',
-      6: 'July',
-      7: 'August',
-      8: 'September',
-      9: 'October',
-      10: 'November',
-      11: 'December'
+            1: 'February',
+            2: 'March',
+            3: 'April',
+            4: 'May',
+            5: 'June',
+            6: 'July',
+            7: 'August',
+            8: 'September',
+            9: 'October',
+            10: 'November',
+            11: 'December'
         };
     }
 
