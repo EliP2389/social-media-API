f68a27d54cb7c2d884304ecf65bb9f4ada03c87f
Eli Pagan <mbk8689@gmail.com>
2022-02-12 18:41:48 -0500

    user controller updated

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,13 +2,81 @@ const { User } = require('../models');
 const { db } = require('../models/User');
 
 const userController = {
-    // get all users
+    // the functions will go in her as methods
+    // get all pizzas
     getAllUser(req, res) {
         User.find({})
+        // populates all comments
+        .populate({
+            path: 'thoughts',
+            // minus sign in front of the '__v' means we dont want it to be returned
+            select: '-__v'
+        })
+        // removes '__v" field from pizzas
+        .select('-__v')
+        // this method 
+        .sort({ _id: -1 })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
             res.status(400).json(err);
+        });
+    },
+
+    // get one pizza by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            // populates comments with pizzas select by ID
+            path: 'thoughts',
+            // removes v field from comments
+            select: '-__v'
         })
+        // removes v field from pizza
+        .select('-__v')
+        .then(dbUserData => {
+            // If no pizza is found, send 404
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!'});
+                return;
+            }
+            res.json(dbuserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    // create user
+    createPizza({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+    // update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    // delete user
+    deleteUser({ params }, res) {
+        User.findByIdAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
     }
-}
+};
+
+module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -8,7 +8,7 @@ const {
     deleteUser
 } = require('../../controllers/user-controller');
 
-// Set up Get all and POST at /api/pizzas
+// Set up Get all and POST at /api/users
 router
     .route('/')
     .get(getAllUser)
